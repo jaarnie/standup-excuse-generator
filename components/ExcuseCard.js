f8e950393218b278e4d57ab9b4a28d846c29e10c
John Arnold <johnadamarnold@gmail.com>
2022-06-08 18:16:05 +0100

    Remove background. Hopefully fixed GA

--- components/ExcuseCard.js
+++ components/ExcuseCard.js
@@ -8,14 +8,14 @@ export default function ExcuseCard({ today, excuse }) {
           {setDate(today ? 'today' : 'yesterday')}
         </small>
       </div>
-      <div className='h-48 justify-center items-center p-4 mb-8 border rounded-lg bg-white'>
+      <div className='h-48 justify-center items-center p-4 mb-8 border-2 border-pink-400 rounded-lg bg-white'>
         <div className='text-center'>
           <p className='uppercase font-bold text-pink-500 mb-2'>
             {today ? 'and today...' : 'yesterday i...'}
           </p>
         </div>
         <div className='flex justify-center z-10'>
-          <p className='m-auto text-2xl uppercase'>{excuse}</p>
+          <p className='m-auto text-2xl'>{excuse}</p>
         </div>
       </div>
     </>
--- constants/index.js
+++ constants/index.js
@@ -1,21 +1,21 @@
 export const YESTERDAY_EXCUSES = [
-  'refactored',
-  'worked on my ticket',
-  'was troubleshooting this thing',
-  "had a huge headache, which i'm sure i mentioned",
-  'was researching into something that might help my task',
-  'hit a brick wall, but then it came to me',
-  'i was writing documentation because nobody else is doing it',
+  'Refactored',
+  'Worked on my ticket',
+  'Was troubleshooting this thing',
+  "Had a huge headache, which i'm sure i mentioned",
+  'Was researching into something that might help my task',
+  'Hit a brick wall, but then it came to me',
+  'I was writing documentation because nobody else is doing it',
 ];
 
 export const TODAY_EXCUSES = [
-  'i will continue on that',
-  "i'll hopefully finish that up",
-  "i'm gonna talk to the others and see what they say about it",
-  "i'll have it in a workable state",
-  "it'll be done, hopefully",
-  "i'll have to speak to devops for some permissions before carrying on",
-  "i'll get to the bottom of this mystery, even if it kills me",
+  'I will continue on that',
+  "I'll hopefully finish that up",
+  "I'm gonna talk to the others and see what they say about it",
+  "I'll have it in a workable state",
+  "It'll be done, hopefully",
+  "I'll have to speak to devops for some permissions before carrying on",
+  "I'll get to the bottom of this mystery, even if it kills me",
 ];
 
 export const randomNumber = () =>
